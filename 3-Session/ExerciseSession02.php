de628c8edb797c57e0cf1b442604fac69e180074
akishajae <akishajae.angeles@gmail.com>
2024-10-03 17:12:24 +0200

    ex2 session re-started

--- 3-Session/ExerciseSession02.php
+++ 3-Session/ExerciseSession02.php
@@ -4,32 +4,55 @@ session_start();
 
 // create initial array and set session
 $array = array(10, 20, 30);
-$_SESSION['array'] = $array;
+$_SESSION["array"] = $array;
 
-// get new position and value
+$errorValue = false;
 
-if (isset($_POST['new_position']) && isset($_POST['new_value'])) {
-    $new_position = $_POST['new_position'];
-    $new_value = $_POST['new_value'];
-}
+if (isset($_POST["submit"])) {
 
-if (isset($_POST['mod'])) {
-    if (isset($new_position) && isset($new_value)) {
-        if ($new_position < count($array) && $new_position >= 0) {
-            $array[$new_position] = $new_value;
-        } else {
-            echo "<br>Error. The new value can't be in that position.";
-        }
+    // reset error
+    $errorValue = false;
+
+    // get variables
+    $submit = $_POST["submit"];
+    $new_position = $_POST["new_position"];
+    $new_value = $_POST["new_value"];
+
+    // value error
+    if ($new_value == null) {
+        $errorValue = true;
+    }
+
+    switch ($submit) {
+        case "modify":
+            if ($new_position < count($array) && $new_position >= 0) {
+                $array[$new_position] = $new_value;
+            } else {
+                echo "<br>Error. The new value can't be in that position.";
+            }
+            break;
+        case "average":
+            $media = calculateAvg($array);
+            break;
+        case "reset":
+            session_unset();
+            session_destroy();
+            session_abort();
+            header("Refresh:0");
+            break;
     }
 }
 
-if (isset($_POST['avg'])) {
-    function calculateAvg($array)
-    {
-        return number_format(array_sum($array) / count($array), 2, ',', '.');
-    }
+function calculateAvg($array)
+{
+    return number_format(array_sum($array) / count($array), 2, ',', '.');
+}
 
-    $media = calculateAvg($array);
+function errorValue($errorValue)
+{
+    if ($errorValue) {
+        echo "<p style='color:red;'>Error. You have to input a value to be modified.</p>";
+    }
 }
 
 ?>
@@ -66,9 +89,11 @@ if (isset($_POST['avg'])) {
         <input type="number" name="new_value" id="new_value">
 
         <br><br>
-        <input type="submit" value="Modify" name="mod">
-        <input type="submit" value="Average" name="avg">
-        <input type="reset" value="Reset">
+        <input type="submit" value="modify" name="submit">
+        <input type="submit" value="average" name="submit">
+        <input type="submit" value="reset" name="submit">
+
+        <?php errorValue($errorValue); ?>
 
         <p>Current array:
             <?php echo implode(", ", $array); ?>
